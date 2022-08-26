799b76021a0923f4608c17f0d720072aa392ae42
ChibiaSad <magur1997@gmail.com>
2022-08-26 13:41:58 +0300

    сделал домашку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
 public class Main {
     public static void main(String[] args) {
+        //task 1
+        int [] intArray = {1, 2, 3};
+        double [] doubles = {1.57, 7.654, 9.986};
+        String [] strings = {"I", "know", "this", "things"};
 
+        //task 2
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.print(intArray[i]);
+            if (i != intArray.length - 1){
+                System.out.print(", ");
+            } else System.out.println();
+        }
+        for (int i = 0; i < doubles.length; i++) {
+            System.out.print(doubles[i]);
+            if (i != doubles.length - 1){
+                System.out.print(", ");
+            } else System.out.println();
+        }
+        for (int i = 0; i < strings.length; i++) {
+            System.out.print(strings[i]);
+            if (i != strings.length - 1){
+                System.out.print(", ");
+            } else System.out.println();
+        }
+
+        //task 3
+        for (int i = intArray.length - 1; i >= 0; i--) {
+            System.out.print(intArray[i]);
+            if (i != 0){
+                System.out.print(", ");
+            } else System.out.println();
+        }
+        for (int i = doubles.length - 1; i >= 0; i--) {
+            System.out.print(doubles[i]);
+            if (i != 0){
+                System.out.print(", ");
+            } else System.out.println();
+        }
+        for (int i = strings.length - 1; i >= 0; i--) {
+            System.out.print(strings[i]);
+            if (i != 0){
+                System.out.print(", ");
+            } else System.out.println();
+        }
+
+        //task 4
+        for (int i : intArray) {
+            if (i % 2 != 0){
+                i++;
+            }
+            System.out.print(i + " ");
+        }
     }
 }
